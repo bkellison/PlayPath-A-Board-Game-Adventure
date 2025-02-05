e7d3a591b02f2293c61c4185b13c6a76bfb5b3ed
mmiller152 <mmiller152@uiowa.edu>
2025-02-04 18:25:51 -0600

    Added fields to Game class

--- src/main/java/org/example/sdprototype/Game.java
+++ src/main/java/org/example/sdprototype/Game.java
@@ -1,5 +1,11 @@
 package org.example.sdprototype;
 
 public class Game {
-
+    private int gameID;
+    private int gameMode;
+    private int playerID;
+    private boolean cpus;
+    /* ADD ONCE DICE CLASS COMPLETE
+    private Dice dice;
+     */
 }
