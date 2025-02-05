70446951c6c5dcefd826a8fb81cbee08e519eeb0
mmiller152 <mmiller152@uiowa.edu>
2025-02-04 18:58:41 -0600

    Constructor for Player class

--- src/main/java/org/example/sdprototype/Player.java
+++ src/main/java/org/example/sdprototype/Player.java
@@ -4,4 +4,13 @@ public class Player {
     private int playerID;
     private int[] location;
     private boolean turn;
+
+    /*
+    Constructor for Player class
+     */
+    public Player(int playerID, int[] location, boolean turn) {
+        this.playerID = playerID;
+        this.location = location;
+        this.turn = turn;
+    }
 }
