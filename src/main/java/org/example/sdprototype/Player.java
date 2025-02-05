e2aadd0fead6af08c6e36156bcc2a8c36c6b7440
mmiller152 <mmiller152@uiowa.edu>
2025-02-04 19:01:08 -0600

    Getters and setters for player ID, player location, and if it is their turn

--- src/main/java/org/example/sdprototype/Player.java
+++ src/main/java/org/example/sdprototype/Player.java
@@ -13,4 +13,22 @@ public class Player {
         this.location = location;
         this.turn = turn;
     }
+
+    /*
+    Getter and setter for player ID
+     */
+    public int getPlayerID() {return playerID;}
+    public void setPlayerID(int playerID) {this.playerID = playerID;}
+
+    /*
+    Getter and setter for player's location on game board
+     */
+    public int[] getLocation() {return location;}
+    public void setLocation(int[] location) {this.location = location;}
+
+    /*
+    Getter and setter for if it is the player's turn
+     */
+    public boolean getTurn() {return turn;}
+    public void setTurn(boolean turn) {this.turn = turn;}
 }
