114c0c3cec3448a7551429854e03511d61c97b3d
mmiller152 <mmiller152@uiowa.edu>
2025-02-17 10:16:26 -0600

    Fixed JUnit dependency issue

--- src/test/java/org/example/sdprototype/PlayerTest.java
+++ src/test/java/org/example/sdprototype/PlayerTest.java
@@ -1,8 +1,8 @@
 package org.example.sdprototype;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
@@ -19,6 +19,26 @@ class PlayerTest {
         assertEquals(1, testPlayer.getPlayerID(), "Player ID should be 1, but was " + testPlayer.getPlayerID());
         assertArrayEquals(new int[]{0,1}, testPlayer.getLocation(), "Location should be {0, 1}, but was " + Arrays.toString(testPlayer.getLocation()));
         assertFalse(testPlayer.getTurn(), "Turn should be false");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, '1,1', true",
+            "3, '2,5', false"
+    })
+    void testPlayerSetters(int id, String locationStr, boolean turn) {
+        // Convert the string into an int array
+        int[] location = Arrays.stream(locationStr.split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        testPlayer.setPlayerID(id);
+        testPlayer.setLocation(location);
+        testPlayer.setTurn(turn);
+
+        assertEquals(id, testPlayer.getPlayerID(), "Player ID should be " + id + ", but was " + testPlayer.getPlayerID());
+        assertArrayEquals(location, testPlayer.getLocation(), "Location should be " + Arrays.toString(location) + ", but was " + Arrays.toString(testPlayer.getLocation()));
+        assertEquals(turn, testPlayer.getTurn(), "Turn should be " + turn + ", but was " + testPlayer.getTurn());
     }
 
     @org.junit.jupiter.api.AfterEach
